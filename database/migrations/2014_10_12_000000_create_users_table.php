be73c8ac0df5d6b9cfead60f02d4ee0f2623ed40
sprguillen <simon@apimo.io>
2019-03-27 23:27:46 +0800

    hpo-v2 API application backbone

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code', 20);
+            $table->string('global_prefix', 40)->nullable()->default(null);
+            $table->enum('type', ['admin', 'client', 'processor', 'staff']);
+            $table->string('payment_mode', 45)->nullable()->default(null);
+            $table->string('dispatch_mode', 45)->nullable()->default(null);
+            $table->string('username', 45)->unique();
+            $table->string('email', 45)->unique();
+            $table->text('password');
+            $table->string('api_token', 80)->unique()->nullable()->default(null);
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2019_03_27_144126_create_user_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('first_name', 45);
+            $table->string('last_name', 45);
+            $table->text('business_name');
+            $table->text('business_address')->nullable()->default(null);
+            $table->string('contact_number', 40);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
