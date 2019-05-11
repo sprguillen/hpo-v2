f122fbd218188c68a04f197f8dd83823f76c332f
Goper Leo Zosa <goper.zosa@gmail.com>
2019-05-11 21:18:36 +0800

    Fix model timezone when created, add unit test to prove newly data are on right timezone delared on environment variable.

--- tests/Api/App/ModelTest.php
+++ tests/Api/App/ModelTest.php
@@ -5,8 +5,10 @@ namespace Tests\Api\App;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Passport\Passport;
-use Tests\TestCase;
 use App\Models\User;
+use Tests\TestCase;
+use Carbon\Carbon;
+use Hash;
 
 class ModelTest extends TestCase
 {
@@ -49,7 +51,7 @@ class ModelTest extends TestCase
     {
         $this->loggedUserClient();
         Passport::actingAs($this->user);
-        
+
         // Check on `api/user/me` route
         $response = $this->json('GET', route('api.user.me'));
         $data = $response->getData();
@@ -65,5 +67,33 @@ class ModelTest extends TestCase
         $this->assertEquals($data->user->id, $this->user->id);
         $this->assertObjectNotHasAttribute('password', $data->user);
         $this->assertObjectNotHasAttribute('remember_token', $data->user);
+    }
+
+    /**
+     * @test
+     */
+    public function makeSureCreatedModelIsRightTimeZone()
+    {
+        $user = new User();
+        $user->global_prefix = '';
+        $user->username = $this->faker->userName;
+        $user->email = $this->faker->freeEmail;
+        $user->password = Hash::make('secret');
+        $user->first_name = $this->faker->firstNameMale;
+        $user->last_name = $this->faker->lastName;
+        $user->contact_number = $this->faker->phoneNumber;
+        $user->business_name = $this->faker->company;
+        $user->business_address = $this->faker->address;
+        $user->is_active = true;
+        $user->save();
+
+        $newUser = User::find($user->id);
+        $created_at = $newUser->created_at;
+
+        // CHeck timezone
+        $this->assertEquals($created_at->tzName, env('APP_TIMEZONE', 'Asia/Manila'));
+
+        // Check environment or config app is on `Asia/Manila` timezone
+        $this->assertEquals(config('app.timezone'), env('APP_TIMEZONE', 'Asia/Manila'));
     }
 }
