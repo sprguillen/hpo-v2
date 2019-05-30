67262632ab00b4426073d49e5f6a14c75693c6e4
Warren Arendain <arendainwm@gmail.com>
2019-05-30 21:28:58 +0800

    Application setup

--- resources/js/utils/constants.js
+++ resources/js/utils/constants.js
@@ -1,3 +1,3 @@
 export const CLIENT_ID = 2;
-export const CLIENT_SECRET = 'iUdu5Z9mHKdtH4oooLYi493bU3JG9M3S98OaINR3';
+export const CLIENT_SECRET = 'cnr3HBAVKvQoea3S5RnrTn83pmMaVHy333sOrET5';
 export const GRANT_TYPE = 'password';
